6b1047f11123bb0184c277aa8dbe3beef7a92e06
Adrian Schneider <siradrian@gmail.com>
2012-06-05 14:36:22 -0700

    generators

---
+++ includes/vde/commands/ContentGenerateForums.php
@@ -0,0 +1,137 @@
+<?php
+
+class VDE_Command_ContentGenerateForums extends VDE_Command
+{
+    static protected $name = 'content:generate:forums';
+    static protected $description = "Generates new forums.";
+    
+    static protected $arguments = array(
+        array('filename', self::ARG_OPTIONAL, 'The filename to import, or null to enter your own content.'),
+        array('root', self::ARG_OPTIONAL, 'The parent forum ID')
+    );
+    
+    protected $defaults = array(
+        'title' => '',
+        'description' => '',
+        'link' => '',
+        'displayorder' => '1',
+        'parentid' => 'root',
+        'daysprune' => '-1',
+        'defaultsortfield' => 'lastpost',
+        'defaultsortorder' => 'desc',
+        'showprivate' => '0',
+        'newpostemail' => '',
+        'newthreademail' => '',
+        'options' =>
+        array (
+            'moderatenewpost' => '0',
+            'moderatenewthread' => '0',
+            'moderateattach' => '0',
+            'styleoverride' => '0',
+            'canhavepassword' => '1',
+            'cancontainthreads' => '1',
+            'active' => '1',
+            'allowposting' => '1',
+            'indexposts' => '1',
+            'allowhtml' => '0',
+            'allowbbcode' => '1',
+            'allowimages' => '1',
+            'allowsmilies' => '1',
+            'allowicons' => '1',
+            'allowratings' => '1',
+            'countposts' => '1',
+            'showonforumjump' => '1',
+            'prefixrequired' => '0',
+        ),
+        'styleid' => '-1',
+        'imageprefix' => '',
+        'password' => '',
+    );
+    
+    public function run($filename = null, $root = -1)
+    {
+        $this->defaults['parentid'] = $root;
+        
+        if ($filename) {
+            if (!file_exists($filename)) {
+                throw new VDE_CLI_Exception("$filename does not exist");
+            }
+            $content = file_get_contents($filename);
+        } else {
+            $content = $this->getUserInput();
+        }
+        
+        foreach (array_map('rtrim', explode("\n", $content)) as $index => $line) {
+            if (!trim($line)) {
+                continue;
+            }
+            
+            $forum = $this->defaults;
+            
+            // No Parent
+            if (preg_match("/^([a-z0-9]+)/i", $line)) {
+                $forum['options']['cancontainthreads'] = 0;
+            } else {
+                $forum['parentid'] = $last['forumid'];
+            }
+            
+            $forum['title'] = trim($line);
+            
+            $forumdata    = datamanager_init('Forum', $this->registry, ERRTYPE_ARRAY);
+            $forum_exists = false;
+            
+            foreach ($forum as $varname => $value) {
+                if ($varname == 'options') {
+                    foreach ($value AS $key => $val) {
+                        $forumdata->set_bitfield('options', $key, $val);
+                    }
+                } else {
+                    $forumdata->set($varname, $value);
+                }
+            }
+            
+            $forumdata->pre_save();
+            if ($forumdata->errors) {
+                throw new Exception('An error occured.  Please refer to the documentation for help.  Error: ' . implode(', ', $forumdata->errors));
+            }
+            
+            $forum['forumid'] = $forumdata->save();
+            
+            $this->writeln("Imported forum " . $this->fgColor('green', $forum['title']));
+            
+            if ($forum['parentid'] == $root) {
+                $last = $forum;
+            } 
+        }
+        
+        $this->writeln("Forums added successfully.  You should rebuild your forum cache now.");
+    }
+    
+    /**
+     * Prompts the user for input
+     * @return    string        User input
+     */
+    protected function getUserInput()
+    {
+        $this->writeln("Enter the forums to add, in the following format:");
+        $this->writeln($this->fgColor('brown', implode(PHP_EOL, array(
+            'Forum A',
+            'Forum B',
+            '    Forum C',
+            '    Forum D',
+            'Forum E'
+        ))));
+        
+        $this->writeln("When you are done, leave two empty lines.");
+        
+        while (!feof(STDIN)) {
+            $content .= fgets(STDIN);
+        
+            if (strpos($content, PHP_EOL . PHP_EOL) !== false) {
+                break;
+            }
+        }
+        
+        return trim($content);
+    }
+}
---
+++ includes/vde/commands/ContentGenerateThreads.php
@@ -0,0 +1,163 @@
+<?php
+
+class VDE_Command_ContentGenerateThreads extends VDE_Command
+{
+    static protected $name = 'content:generate:thread';
+    static protected $description = "Generates new threads/posts.";
+    
+    static protected $arguments = array(
+        array('threads',     self::ARG_PROMPT,  'Threads to create?'),
+        array('replies_min', self::ARG_OPTIONAL, 'Replies per thread (min)'),
+        array('replies_max', self::ARG_OPTIONAL, 'Replies per thread (max)'),
+        array('source',      self::ARG_OPTIONAL, 'Path to content file.')
+    );
+    
+    /**
+     * @var    string        Post content
+     */
+    protected $content;
+    
+    /**
+     * @var    string        Allowed forum IDs
+     */
+    protected $forums;
+    
+    /**
+     * Generates threads
+     * @param    integer        Number of threads to create
+     * @param    integer        Number of replies to create [lower boundary]
+     * @param    integer        Number of replies to create [higher boundary]
+     * @param    string|null    Location of post data
+     */
+    public function run($threads, $repliesMin = 0, $repliesMax = 50, $content = null)
+    {
+        $this->content = file_get_contents($content ?: DIR . '/includes/vde/data/posts.txt');
+        $this->forums  = $this->getActiveForums();
+        
+        for ($i = 0; $i < $threads; $i++) {
+            $thread = datamanager_init('Thread_FirstPost', $this->registry, ERRTYPE_ARRAY, 'threadpost');
+            
+            $thread->set('title', $this->getRandomTitle());
+            $thread->set('pagetext', $this->getRandomBody());
+            $thread->set('userid', $this->getRandomUser());
+            $thread->set('ipaddress', '127.0.0.1');
+            $thread->set('allowsmilie', false);
+            $thread->set('visible', 1);
+            $thread->set('dateline', $time = TIMENOW - rand(86400, (86400*45)));
+            $thread->set('forumid', $this->getRandomForum());
+            
+            $thread->pre_save();
+            if (!empty($thread->errors)) {
+                throw new Exception('Error creating thread: ' . implode(', ', $thread->errors));
+            }
+            
+            $threadId = $thread->save();
+            $replies = rand($repliesMin, $repliesMax);
+            
+            for ($i = 0; $i < $replies; $i++) {
+                $reply = datamanager_init('Post', $this->registry, ERRTYPE_ARRAY, 'threadpost');
+                
+                $reply->set('threadid', $threadId);
+                $reply->set('title', '');
+                $reply->set('pagetext', $this->getRandomBody());
+                $reply->set('userid', $this->getRandomUser());
+                $reply->set('ipaddress', '127.0.0.1');
+                $reply->set('allowsmilie', false);
+                $reply->set('visible', 1);
+                $reply->set('dateline', $time += (rand(3, 10000)));
+                
+                $reply->pre_save();
+                if (!empty($reply->errors)) {
+                    throw new Exception('Error creating thread: ' . implode(', ', $reply->errors));
+                }
+                
+                $reply->save();
+            }
+        }
+    }
+    
+    /**
+     * Returns a list of forums that users can post in
+     * @return    array        Forum IDs that allow posting
+     */
+    protected function getActiveForums()
+    {
+        $forums = $this->registry->forumcache;
+        shuffle($forums);
+        
+        $forumIds = array();
+        foreach ($forums as $forum) {
+            if (!($forum['options'] & $this->registry->bf_misc_forumoptions['active'])) {
+                continue;
+            }
+        
+            if (!($forum['options'] & $this->registry->bf_misc_forumoptions['allowposting'])) {
+                continue;
+            }
+            if (!($forum['options'] & $this->registry->bf_misc_forumoptions['cancontainthreads'])) {
+                continue;
+            }
+        
+            $forumIds[] = $forum['forumid'];
+        }
+        
+        return $forumIds;
+    }
+    
+    /**
+     * Generates a random post title
+     * @return    string        Post title
+     */
+    protected function getRandomTitle()
+    {
+        $lines = array_filter(explode("\n", $this->content), 'trim');
+        $words = explode(' ', $lines[array_rand($lines)]);
+        
+        return preg_replace('/([^ a-zA-Z])/', '', implode(' ', array_slice($words, 0, rand(4, 7))));
+    }
+    
+    /**
+     * Generates random post content 
+     * @return    string        Post content
+     */
+    protected function getRandomBody()
+    {
+        $lines = array_filter(explode("\n", $this->content), 'trim');
+        shuffle($lines);
+        
+        $out = '';
+        $paragraphs = rand(1, 6);
+        
+        for ($i = 0; $i < $paragraphs; $i++) {
+            $out .= $lines[$i] . "\n\n";
+        }
+        
+        return trim($out);
+    }
+    
+    /**
+     * Fetches a random user from the database
+     * @return    integer        Random User ID
+     */
+    protected function getRandomUser()
+    {
+        $id = $this->registry->db->query_first("
+            SELECT userid
+              FROM " . TABLE_PREFIX . "user
+            ORDER
+                BY rand()
+             LIMIT 1
+        ");
+        
+        return intval($id['userid']);
+    }
+    
+    /**
+     * Returns a random forum ID that users can post in
+     * @return    integer        Forum ID
+     */
+    protected function getRandomForum()
+    {
+        return $this->forums[array_rand($this->forums)];
+    }
+}
